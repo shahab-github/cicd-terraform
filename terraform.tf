bac708899b6beaccf9e265af45768f55cce0204d
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-11 16:12:42 -0500

    added cloud backend

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,19 @@
 #
 
 terraform {
-    backend "s3" {
-        bucket = "mys3bucket679"
-        key = "dev/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "mydynamodb679"
+    # backend "s3" {
+    #     bucket = "mys3bucket679"
+    #     key = "dev/terraform.tfstate"
+    #     region = "us-east-1"
+    #     dynamodb_table = "mydynamodb679"
+    # }
+
+    backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "cicd-terraform"
+
+    workspaces {
+      name = "cicd-terraform"
     }
+  }
 }
